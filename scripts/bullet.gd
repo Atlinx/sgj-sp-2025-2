dddeb046370c404a20bfdeb720baa93fd4ce1f9b
Ray1212144 <147676185+Ray1212144@users.noreply.github.com>
2025-04-11 17:38:11 -0400

    robbery game

---
+++ scripts/bullet.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+
+
+func _physics_process(delta: float) -> void:
+	position += velocity * delta
+
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("hitbox"):
+		queue_free()
+
+
+func _on_dead_time_timeout() -> void:
+	queue_free()
