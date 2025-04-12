b9be87f9343ea7005ee6e1f5a766b24e66e3c23b
atlinx <atlinuxtm@gmail.com>
2025-04-12 16:49:52 -0400

    Fix broken map scene + duplicated code

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -28,8 +28,6 @@ func _ready() -> void:
 	reparent.call_deferred(get_tree().root)
 	TransitionManager.global.on_transition_out.connect(on_save_state.emit)
 	TransitionManager.global.on_transition_in.connect(on_load_state.emit)
-	TransitionManager.global.on_transition_out.connect(on_save_state.emit)
-	TransitionManager.global.on_transition_in.connect(on_load_state.emit)
 	# Load default style
 	Dialogic.Styles.load_style()
 	Dialogic.Styles.get_layout_node().visible = false
